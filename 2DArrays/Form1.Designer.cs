de7c1026189d7ddfeab5d30744bd373ccb6107e4
Jikrebello <33652252+Jikrebello@users.noreply.github.com>
2021-05-19 23:10:19 +0200

    Add files via upload

---
+++ 2DArrays/Form1.Designer.cs
@@ -0,0 +1,191 @@
+﻿
+namespace _2DArrays
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.TxtDisplay = new System.Windows.Forms.TextBox();
+            this.BtnLoadTable = new System.Windows.Forms.Button();
+            this.BtnChangeFranksSales = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
+            this.BtnAdd10ToAll = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            this.button6 = new System.Windows.Forms.Button();
+            this.button7 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // TxtDisplay
+            // 
+            this.TxtDisplay.Location = new System.Drawing.Point(10, 12);
+            this.TxtDisplay.Multiline = true;
+            this.TxtDisplay.Name = "TxtDisplay";
+            this.TxtDisplay.ScrollBars = System.Windows.Forms.ScrollBars.Both;
+            this.TxtDisplay.Size = new System.Drawing.Size(447, 181);
+            this.TxtDisplay.TabIndex = 1;
+            // 
+            // BtnLoadTable
+            // 
+            this.BtnLoadTable.Location = new System.Drawing.Point(10, 242);
+            this.BtnLoadTable.Name = "BtnLoadTable";
+            this.BtnLoadTable.Size = new System.Drawing.Size(145, 46);
+            this.BtnLoadTable.TabIndex = 0;
+            this.BtnLoadTable.Text = "View Data";
+            this.BtnLoadTable.UseVisualStyleBackColor = true;
+            this.BtnLoadTable.Click += new System.EventHandler(this.BtnLoadTable_Click);
+            // 
+            // BtnChangeFranksSales
+            // 
+            this.BtnChangeFranksSales.Location = new System.Drawing.Point(10, 294);
+            this.BtnChangeFranksSales.Name = "BtnChangeFranksSales";
+            this.BtnChangeFranksSales.Size = new System.Drawing.Size(145, 46);
+            this.BtnChangeFranksSales.TabIndex = 2;
+            this.BtnChangeFranksSales.Text = "Change Franks Sales";
+            this.BtnChangeFranksSales.UseVisualStyleBackColor = true;
+            this.BtnChangeFranksSales.Click += new System.EventHandler(this.BtnChangeFranksSales_Click);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(10, 346);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(145, 46);
+            this.button1.TabIndex = 3;
+            this.button1.Text = "Copy Lucys sales to Karens";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.Button1_Click);
+            // 
+            // BtnAdd10ToAll
+            // 
+            this.BtnAdd10ToAll.Location = new System.Drawing.Point(161, 242);
+            this.BtnAdd10ToAll.Name = "BtnAdd10ToAll";
+            this.BtnAdd10ToAll.Size = new System.Drawing.Size(145, 46);
+            this.BtnAdd10ToAll.TabIndex = 4;
+            this.BtnAdd10ToAll.Text = "Add 10 to all";
+            this.BtnAdd10ToAll.UseVisualStyleBackColor = true;
+            this.BtnAdd10ToAll.Click += new System.EventHandler(this.BtnAdd10ToAll_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(161, 294);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(145, 46);
+            this.button3.TabIndex = 5;
+            this.button3.Text = "Sales less than 1000";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.Button3_Click);
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(161, 346);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(145, 46);
+            this.button4.TabIndex = 6;
+            this.button4.Text = "Highest sales amount of the week";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.Button4_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(312, 242);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(145, 46);
+            this.button2.TabIndex = 7;
+            this.button2.Text = "Weekly Total Sales";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.Button2_Click);
+            // 
+            // button5
+            // 
+            this.button5.Location = new System.Drawing.Point(312, 294);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(145, 46);
+            this.button5.TabIndex = 8;
+            this.button5.Text = "Highest Weekly Sales Person";
+            this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.Button5_Click);
+            // 
+            // button6
+            // 
+            this.button6.Location = new System.Drawing.Point(312, 346);
+            this.button6.Name = "button6";
+            this.button6.Size = new System.Drawing.Size(145, 46);
+            this.button6.TabIndex = 9;
+            this.button6.Text = "Weekly Payout From Commission";
+            this.button6.UseVisualStyleBackColor = true;
+            this.button6.Click += new System.EventHandler(this.button6_Click);
+            // 
+            // button7
+            // 
+            this.button7.Location = new System.Drawing.Point(314, 199);
+            this.button7.Name = "button7";
+            this.button7.Size = new System.Drawing.Size(143, 27);
+            this.button7.TabIndex = 10;
+            this.button7.Text = "Clear";
+            this.button7.UseVisualStyleBackColor = true;
+            this.button7.Click += new System.EventHandler(this.button7_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(469, 400);
+            this.Controls.Add(this.button7);
+            this.Controls.Add(this.button6);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.BtnAdd10ToAll);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.BtnChangeFranksSales);
+            this.Controls.Add(this.TxtDisplay);
+            this.Controls.Add(this.BtnLoadTable);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.TextBox TxtDisplay;
+        private System.Windows.Forms.Button BtnLoadTable;
+        private System.Windows.Forms.Button BtnChangeFranksSales;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button BtnAdd10ToAll;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.Button button6;
+        private System.Windows.Forms.Button button7;
+    }
+}
+
---
+++ 2DArrays/Form1.cs
@@ -0,0 +1,266 @@
+﻿using System;
+using System.Globalization;
+using System.IO;
+using System.Windows.Forms;
+
+namespace _2DArrays
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            LoadData();
+            GetWeeklyTotals();
+        }
+
+        private int[,] tableValues = new int[4, 6];
+        private string[] weekDays = new string[6];
+        private string[] salesPeople = new string[4];
+        private float[] commissionPerDay = new float[6];
+        private float[] commissionPerSalesPerson = new float[4];
+        int[] totalWeeklySales = new int[4];
+
+
+        // 1) Gets the data from files and displays it as a table in the text box
+        private void BtnLoadTable_Click(object sender, EventArgs e) => DisplayData();
+        private void LoadData()
+        {
+            TxtDisplay.Text = "";
+            string path = Application.StartupPath;
+            const string TABLEVALUES_PATH = @"\table_values.txt";
+            const string WEEKDAYS_PATH = @"\daysOfTheWeek.txt";
+            const string SALESPEOPLE_PATH = @"\salesPeople.txt";
+            const string COMMISSION_PATH = @"\commission_weighting.txt";
+
+            // fills the tableValues Array
+            using (StreamReader reader = new StreamReader(path + TABLEVALUES_PATH))
+            {
+                for (int i = 0; i < 4; i++)
+                {
+                    for (int j = 0; j < 6; j++)
+                    {
+                        tableValues[i, j] = Convert.ToInt32(reader.ReadLine());
+                    }
+                }
+                reader.Close();
+            }
+
+            // fills the weekDays Array
+            using (StreamReader reader = new StreamReader(path + WEEKDAYS_PATH))
+            {
+                for (int i = 0; i < weekDays.Length; i++)
+                {
+                    weekDays[i] = reader.ReadLine();
+                }
+                reader.Close();
+            }
+
+            // fills the salesPeople Array
+            using (StreamReader reader = new StreamReader(path + SALESPEOPLE_PATH))
+            {
+                for (int i = 0; i < salesPeople.Length; i++)
+                {
+                    salesPeople[i] = reader.ReadLine();
+                }
+                reader.Close();
+            }
+
+            // fills the commission array
+            using (StreamReader reader = new StreamReader(path + COMMISSION_PATH))
+            {
+                for (int i = 0; i < commissionPerDay.Length; i++)
+                {
+                    commissionPerDay[i] = float.Parse(reader.ReadLine(), CultureInfo.InvariantCulture);
+                }
+                reader.Close();
+            }
+        }
+        private void DisplayData()
+        {
+            // Display the table in TxtDisplay
+            TxtDisplay.Text += "\t";
+
+            // set the days of the week/ columns
+            for (int i = 0; i < weekDays.Length; i++)
+            {
+                TxtDisplay.Text += weekDays[i].ToString() + "\t";
+            }
+
+            TxtDisplay.Text += Environment.NewLine;
+
+            // set the rows (salesPeople), to go underneath the columns (weekdays), then drop down to the next row
+            for (int i = 0; i < 4; i++)
+            {
+                TxtDisplay.Text += salesPeople[i] + "\t";
+
+                for (int j = 0; j < 6; j++)
+                {
+                    TxtDisplay.Text += tableValues[i, j] + "\t";
+                }
+
+                TxtDisplay.Text += Environment.NewLine;
+            }
+
+            TxtDisplay.Text += Environment.NewLine;
+        }
+
+        // 2) Changes franks sales on Thursday to 1500
+        private void BtnChangeFranksSales_Click(object sender, EventArgs e)
+        {
+            tableValues[2, 3] = 1500;
+            DisplayData();
+        }
+
+        // 3) Copy Lucy's sales for Thursday ({1, 2] = 900) into Karen's sales ([3, 0] = 1080) for Wednesday
+        private void Button1_Click(object sender, EventArgs e)
+        {
+            tableValues[3, 0] = tableValues[1, 2];
+            DisplayData();
+        }
+
+        // 4) Add 10 to all the sales.
+        private void BtnAdd10ToAll_Click(object sender, EventArgs e)
+        {
+            TxtDisplay.Text += Environment.NewLine + "Added 10 to all table values" + Environment.NewLine;
+
+            for (int i = 0; i < 4; i++)
+            {
+                for (int j = 0; j < 6; j++)
+                {
+                    tableValues[i, j] += 10;
+                }
+            }
+
+            DisplayData();
+        }
+
+        // 5) Display the names of all salespeople and dates for which sales were below 1000.
+        private void Button3_Click(object sender, EventArgs e)
+        {
+            TxtDisplay.Text += Environment.NewLine + "All people who had sales lower than 1000:" + Environment.NewLine;
+
+            for (int i = 0; i < 4; i++)
+            {
+                for (int j = 0; j < 6; j++)
+                {
+                    if (tableValues[i, j] < 1000)
+                    {
+                        TxtDisplay.Text += salesPeople[i] + "\t" + weekDays[j] + Environment.NewLine;
+                    }
+                }
+            }
+        }
+
+        // 6) Display the largest sales amount and the corresponding salesperson’s name and date.
+        private void Button4_Click(object sender, EventArgs e)
+        {
+            TxtDisplay.Text += Environment.NewLine + "Largest sales amount and salesperson’s name and date." + Environment.NewLine;
+
+            int largestSalesAmount = -1;
+            string largestSalesName = "";
+            string largestWeekName = "";
+
+            for (int i = 0; i < 4; i++)
+            {
+                for (int j = 0; j < 6; j++)
+                {
+                    if (tableValues[i, j] > largestSalesAmount)
+                    {
+                        largestSalesAmount = tableValues[i, j];
+                        largestSalesName = salesPeople[i];
+                        largestWeekName = weekDays[j];
+                    }
+                }
+            }
+
+            TxtDisplay.Text += "Top sales person: " + largestSalesName + "\t" + "Top sales amount: " + largestSalesAmount + "\t" + "Achieved on " + largestWeekName + Environment.NewLine;
+        }
+
+        // 7) Determine the weekly sales totals for each salesperson. Make sure you store each total in a one dimensional array called WeeklySales.
+        private void Button2_Click(object sender, EventArgs e)
+        {
+            TxtDisplay.Text += Environment.NewLine + "Sales Person Total Sales:" + Environment.NewLine;
+
+
+            for (int i = 0; i < 4; i++)
+            {
+                TxtDisplay.Text += "Sales person: " + salesPeople[i] + "\t\tWeekly Total: R " + totalWeeklySales[i] + Environment.NewLine;
+            }
+
+            TxtDisplay.Text += Environment.NewLine;
+        }
+        private void GetWeeklyTotals()
+        {
+            int weeklyTotal = 0;
+
+            TxtDisplay.Text += Environment.NewLine;
+
+            for (int i = 0; i < 4; i++)
+            {
+                for (int j = 0; j < 6; j++)
+                {
+                    weeklyTotal += tableValues[i, j];
+                    totalWeeklySales[i] = weeklyTotal;
+                }
+            }
+        }
+
+        // 8) which sales person had the greatest weekly sales from the calculations.
+        private void Button5_Click(object sender, EventArgs e)
+        {
+            int weeklyTotal = 0;
+            string weeklyName = "";
+
+            for (int i = 0; i < 4; i++)
+            {
+                if (totalWeeklySales[i] > weeklyTotal)
+                {
+                    weeklyTotal = totalWeeklySales[i];
+                    weeklyName = salesPeople[i];
+                }
+            }
+
+            TxtDisplay.Text += Environment.NewLine + "Highest Sales person: " + weeklyName + "\tHighest Weekly Total: R " + weeklyTotal + Environment.NewLine;
+        }
+
+        /* 9) All salespeople are paid by commission. The store has determined through many years of
+         * study that some days it is easier to make sales than others, and thus, uses different
+         * commission rates for different days.
+         * Mon - 12%
+         * Tues - 11.23%
+         * Wed - 10.08%
+         * Thurs - 8.5%
+         * Fri - 8%
+         * Sat - 7.64%
+         * Determine the weeks pay for each salesperson*/
+        private void button6_Click(object sender, EventArgs e) => GetSalesPersonSalary();
+        private void GetSalesPersonSalary()
+        {
+            // Loop 
+            for (int i = 0; i < 4; i++)
+            {
+                // 
+                for (int j = 0; j < 6; j++)
+                {
+                    commissionPerSalesPerson[i] += tableValues[i, j] * commissionPerDay[i];
+                }
+            }
+
+            TxtDisplay.Text += Environment.NewLine + "Sales Person Weekly Salary:" + Environment.NewLine;
+
+            for (int i = 0; i < 4; i++)
+            {
+                TxtDisplay.Text += "Sales person: " + salesPeople[i] + "\t\tWeekly Pay: R " + Math.Round(commissionPerSalesPerson[i], 2) + Environment.NewLine;
+            }
+
+            TxtDisplay.Text += Environment.NewLine;
+        }
+
+        // Clears the text box
+        private void button7_Click(object sender, EventArgs e)
+        {
+            TxtDisplay.Text = "";
+        }
+    }
+}
